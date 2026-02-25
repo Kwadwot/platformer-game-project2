b86d2a8f84401dd80bb056a4bcbf97183ae72e95
Kwadwot <kwadwoptwumasi@gmail.com>
2026-02-25 03:01:14 -0500

    upgrage gameloop with update logic

--- src/main/Game.java
+++ src/main/Game.java
@@ -5,6 +5,7 @@ public class Game implements Runnable{
     private GamePanel gamePanel;
     private Thread gameThread;
     private final int FPS_SET = 120;
+    private final int UPS_SET = 200;
 
     public Game() {
 //        System.out.println("I am alive!");
@@ -19,30 +20,49 @@ public class Game implements Runnable{
         gameThread.start();
     }
 
+    public void update() {
+        gamePanel.updateGame();
+    }
+
     @Override
     public void run() {
 
         double timePerFrame = 1000000000.0 / FPS_SET;
-        long now = System.nanoTime();
-        long lastFrame = System.nanoTime();
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+
+        long previousTime = System.nanoTime();
+
         int frames = 0;
+        int updates = 0;
         long lastCheck = System.currentTimeMillis();
 
+        double deltaU = 0;
+        double deltaF = 0;
+
         while(true) {
+            long currentTime = System.nanoTime();
 
-            now = System.nanoTime();
-            if (System.nanoTime() - lastFrame >= timePerFrame) {
+            deltaU += (currentTime - previousTime) / timePerUpdate;
+            deltaF += (currentTime - previousTime) / timePerFrame;
+            previousTime = currentTime;
 
+            if (deltaU >= 1) {
+                update();
+                updates++;
+                deltaU--;
+            }
+
+            if (deltaF >= 1) {
                 gamePanel.repaint();
-                lastFrame = now;
+                deltaF--;
                 frames++;
             }
 
-
             if (System.currentTimeMillis() - lastCheck >= 1000) {
                 lastCheck = System.currentTimeMillis();
-                System.out.println("FPS " + frames);
+                System.out.println("FPS: " + frames + "| UPS: " + updates);
                 frames = 0;
+                updates = 0;
             }
         }
     }
